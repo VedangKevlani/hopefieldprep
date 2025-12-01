dafc745fa5aa9f38c5a9a02cfbf68799028e8d09
Vedang Kevlani <kevlanivedang28@gmail.com>
2025-12-01 16:40:46 -0500

    Added an Image Gallery

--- hopefield-frontend/src/components/ImageGallery.tsx
+++ hopefield-frontend/src/components/ImageGallery.tsx
@@ -13,7 +13,7 @@ export default function ImageGallery() {
 
   return (
     <section className="py-10 px-6">
-      <h2 className="text-3xl font-bold text-center text-[#FF3B3B] mb-8">
+      <h2 className="text-3xl font-bold text-center text-[#EAC30E] mb-8">
         Our Campus
       </h2>
 
--- hopefield-frontend/src/data/slideshowData.ts
+++ hopefield-frontend/src/data/slideshowData.ts
@@ -1,7 +1,7 @@
 // src/data/slideshowData.ts
 export const slides = [
   {
-    image: "/images/Hopefield-thenest.jpg", // put your images in public/images/
+    image: "/images/Hopefield-jamaicaday.jpg", // put your images in public/images/
     text: "Home of the Hopefielders!",
   },
   {

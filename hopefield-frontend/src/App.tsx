44ee3ce6e6babd528ce7a3c43eab72368014131a
Vedang Kevlani <kevlanivedang28@gmail.com>
2025-12-02 17:38:37 -0500

    crud for pdfs

--- hopefield-frontend/src/App.tsx
+++ hopefield-frontend/src/App.tsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 import AdminStaffPage from "./pages/AdminStaffPage"; 
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
-import PdfManager from "./pages/pdfManager";
+import PdfManager from "./pages/PDF-Manager";
 
 function App() {
   return (

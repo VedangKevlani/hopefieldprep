0a1afe7253931511c1124c1e1bb7cc4657b51ddd
Vedang Kevlani <kevlanivedang28@gmail.com>
2025-12-04 23:14:54 -0500

    pdf preview fix

--- hopefield-frontend/src/pages/AdminAdmissions.tsx
+++ hopefield-frontend/src/pages/AdminAdmissions.tsx
@@ -2,7 +2,7 @@
 import { useRef, useEffect, useState } from "react";
 import * as pdfjsLib from "pdfjs-dist";
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";;
+pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
 
 type PdfPreviewProps = {
   fileUrl?: string;

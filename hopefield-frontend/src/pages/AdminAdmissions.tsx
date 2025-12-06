d193b67eed55e8b018e400eabf53065d6b197883
VedangKevlani <kevlanivedang28@gmail.com>
2025-12-05 21:41:50 -0500

    moving files to backend pdfs

--- hopefield-frontend/src/pages/AdminAdmissions.tsx
+++ hopefield-frontend/src/pages/AdminAdmissions.tsx
@@ -10,7 +10,7 @@ type PdfPreviewProps = {
 };
 
 export default function PdfPreview({
-fileUrl = "https://hopefield-backend.onrender.com/uploads/Hopefield-Prep-Application-form.pdf",
+fileUrl = "https://hopefield-backend.onrender.com/uploads/",
   className = "",
 }: PdfPreviewProps) {
   const containerRef = useRef<HTMLDivElement | null>(null);

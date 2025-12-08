04e308b159444ee769ceed34dbc2d81349d34ddd
VedangKevlani <kevlanivedang28@gmail.com>
2025-12-07 22:17:36 -0500

    submenu

--- hopefield-frontend/src/pages/Home.tsx
+++ hopefield-frontend/src/pages/Home.tsx
@@ -2,16 +2,29 @@ import AchievementsSection from "../components/AchievementsSection";
 import InteractiveCalendar from "../components/InteractiveCalendar";
 import SchoolStats from "../components/SchoolStats";
 import AdmissionBanner from "../components/AdmissionBanner";
+import Slideshow from "../components/Slideshow";
 
 export default function Home() {
   return (
     <>
+    {/* Slideshow Section */}
+    <Slideshow />
+
       {/* Achievements Section */}
       <AchievementsSection
       achievements={[
-        { title: "Academic Excellence", description: "Top grades across subjects." },
-        { title: "Creative Arts", description: "Fostering imagination and creativity." },
-        { title: "Sports Achievements", description: "Winning in interschool competitions." },
+        { title: "PEP Performance", description:"In the 2025 PEP sitting, 70% of our Grade-4 students achieved “mastery” in Numeracy, and 67% achieved “mastery” in Literacy.",
+ },
+        { title: "First-Choice Secondary Placements", description:"Over 89% of our graduates were placed in one of their top-choice high schools after PEP.",
+ },
+        { title: "Academic Awards & Competitions", description: "Students have earned top positions in national math and science competitions, achieving distinctions at regional Olympiads and school challenge quizzes.",
+},
+{ title: "Clubs & Extracurricular Life", description:     "From robotics, chess, art, music, drama, dance, homework, and sports, students actively participate in clubs that nurture creativity, teamwork, discipline, and real-world skills beyond academics.",
+},
+{ title: "Diversity & Belonging", description:     "Hopefield fosters a community where every child feels welcomed, respected, and valued. We celebrate cultural diversity, personal identity, and encourage empathy, mutual respect, and collaboration.",
+},
+{ title: "Holistic Personal Development", description:     "Our curriculum focuses not only on academic excellence, but on character, leadership, emotional well-being, confidence, and social responsibility — shaping students to thrive in every aspect of life.",
+}
       ]}
       icons={[
         "/images/puzzle.png",
